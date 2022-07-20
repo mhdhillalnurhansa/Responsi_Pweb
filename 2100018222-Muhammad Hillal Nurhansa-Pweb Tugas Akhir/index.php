1fa281e9865b31cebf5d814158ddc1a4c9ea27a7
mhdhillalnurhansa <101721261+mhdhillalnurhansa@users.noreply.github.com>
2022-07-20 12:34:41 +0700

    Add files via upload

---
+++ 2100018222-Muhammad Hillal Nurhansa-Pweb Tugas Akhir/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style type="text/css">
+    body{
+      font-family: times new roman;
+      background-image: url(foto1.jpg);
+    }
+    h1{
+    color: navy;
+    }
+    h3{
+    color: navy;
+  }
+
+    }
+  </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MENU</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+  <h1><center>Selamat Datang Di Menu</center></h1>
+<h3><center>Silahkan pilih menu yang ingin Anda buka</center></h3>
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link" href="kalkulator.html">KALKULATOR</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="kalender.php">KALENDER</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="form.html">FORM</a>
+  </li>
+</ul>
+</html>
---
+++ 2100018222-Muhammad Hillal Nurhansa-Pweb Tugas Akhir/kalender.php
@@ -0,0 +1,71 @@
+<body background="foto4.png">
+<?php
+$hari=date("d");
+$hariini=$hari;
+$bulan=date("m");
+$tahun=date("Y");
+
+$jumlahhari=date("t",mktime(0,0,0,$bulan,$hari,$tahun));
+?>
+<br><br>
+
+<?php
+switch($bulan){
+	case 1: $nmbulan = "Jan";break;
+	case 2: $nmbulan = "Feb";break;
+	case 3: $nmbulan = "Mar";break;
+	case 4: $nmbulan = "Apr";break;
+	case 5: $nmbulan = "Mei";break;
+	case 6: $nmbulan = "Jun";break;
+	case 7: $nmbulan = "Jul";break;
+	case 8: $nmbulan = "Agu";break;
+	case 9: $nmbulan = "Sep";break;
+	case 10: $nmbulan = "Okt";break;
+	case 11: $nmbulan = "Nov";break;
+	case 12: $nmbulan = "Des";break;
+}
+echo"<center><h1>$nmbulan $tahun </h1></center>";?>
+<br>
+<table style="border: 2px solid #1E90FF" align="center" cellpadding="10">
+<tr bgcolor="#ADD8E6">
+	<td align="center"><font color="#FF0000">Min</font></td>
+	<td align="center">Sen</td>
+	<td align="center">Sel</td>
+	<td align="center">Rab</td>
+	<td align="center">Kam</td>
+	<td align="center">Jum</td>
+	<td align="center">Sab</td>
+</tr>
+<br>
+<?php
+$s=date("w",mktime(0,0,0,$bulan,1,$tahun));
+for ($ds=1; $ds<=$s ; $ds++) {
+	echo"<td></td>" ;
+}
+
+for($d=1;$d<=$jumlahhari;$d++){
+	if(date("w",mktime(0,0,0,$bulan,$d,$tahun))==0){
+		echo"<tr>";
+	}
+
+	$warna="#000000";
+	$warnasel="#ffffff";
+
+	if(date("l",mktime(0,0,0,$bulan,$d,$tahun))=="Sunday"){
+		$warna="#FF0000";
+	}
+
+	//blok sel yang sesuai hari ini
+	if($hariini == $d){
+		$warna="white";
+		$warnasel="grey";
+	}
+
+	echo" <td align=center valign=middle bgcolor=$warnasel> <span style=\"color: $warna\"> $d </span></td>";
+	if(date("w",mktime(0,0,0,$bulan,$d,$tahun))==6){
+		echo"</tr>";
+	}
+}
+echo'</table>';
+?>
+</body>
